51d47b989b4fa0ee525a3a252b55515ee2fd70e2
willdevps <willgamer642@gmail.com>
2023-09-14 11:02:52 -0300

    tabela usuarios e post

--- database/migrations/2023_09_14_133713_create_posts_table.php
+++ database/migrations/2023_09_14_133713_create_posts_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('usuario');
-            $table->string('foto');
-            $table->string('nome');
-            $table->string('email')->unique();
-            $table->string('senha');
-            $table->rememberToken();
+            $table->foreignIdFor(User::class)->constrained(); //chave estrangeira de usuários
+            $table->string('mensagem');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
